50ebf0b6b690a56199447238656e2df15a70ca71
Jeremy Magland <jeremy.magland@gmail.com>
2021-07-05 21:58:49 -0400

    fix unit visibility

--- src/python/sortingview/gui/extensions/mountainview/MVSortingView/VisibleUnitsControl.tsx
+++ src/python/sortingview/gui/extensions/mountainview/MVSortingView/VisibleUnitsControl.tsx
@@ -1,5 +1,6 @@
 import { Checkbox } from '@material-ui/core';
-import React, { FunctionComponent, useCallback, useState } from 'react';
+import { useSortingInfo } from 'python/sortingview/gui/pluginInterface/useSortingInfo';
+import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
 import { Recording, Sorting, SortingCuration, SortingSelection, SortingSelectionDispatch } from "../../../pluginInterface";
 
 type Props = {
@@ -15,10 +16,12 @@ type Props = {
 const VisibleUnitsControl: FunctionComponent<Props> = ({ sorting, recording, selection, selectionDispatch, curation }) => {
     const [hideRejected, setHideRejected] = useState(false)
     const [showAcceptedOnly, setShowAcceptedOnly] = useState(false)
-    const [restrictToVisibleElectrodes, setRestrictToVisibleElectrodes] = useState(true)
+    // const [restrictToVisibleElectrodes, setRestrictToVisibleElectrodes] = useState(true)
     const visibleElectrodeIds = selection.visibleElectrodeIds
 
     // const {result: peakElectrodeIds} = useHitherJob<PeakElectrodeIds>('createjob_get_peak_channels', {sorting_object: sorting.sortingObject, recording_object: recording.recordingObject}, {useClientCache: true})
+
+    const sortingInfo = useSortingInfo(sorting.sortingPath)
 
     const handleShowAcceptedOnly = useCallback(() => {
         setShowAcceptedOnly(!showAcceptedOnly)
@@ -26,43 +29,43 @@ const VisibleUnitsControl: FunctionComponent<Props> = ({ sorting, recording, sel
     const handleHideRejected = useCallback(() => {
         setHideRejected(!hideRejected)
     }, [hideRejected])
-    const handleRestrictToVisibleElectrodes = useCallback(() => {
-        setRestrictToVisibleElectrodes(!restrictToVisibleElectrodes)
-    }, [restrictToVisibleElectrodes])
+    // const handleRestrictToVisibleElectrodes = useCallback(() => {
+    //     setRestrictToVisibleElectrodes(!restrictToVisibleElectrodes)
+    // }, [restrictToVisibleElectrodes])
     
-    // the following is causing a problem
-    // useEffect(() => {
-    //     const includeUnit = (uid: number) => {
-    //         if ((restrictToVisibleElectrodes) && (visibleElectrodeIds) && (peakElectrodeIds)) {
-    //             const peakElectrodeId = peakElectrodeIds[uid + '']
-    //             if (!visibleElectrodeIds.includes(peakElectrodeId)) return false
-    //         }
-    //         if (showAcceptedOnly) {
-    //             return getLabelsForUnitId(uid, curation).includes('accept')
-    //         }
-    //         else if (hideRejected) {
-    //             return !getLabelsForUnitId(uid, curation).includes('reject')
-    //         }
-    //         else {
-    //             return true
-    //         }    
-    //     }
-    //     const visibleUnitIds = (sortingInfo?.unit_ids || []).filter(uid => includeUnit(uid))
-    //     selectionDispatch({type: 'SetVisibleUnitIds', visibleUnitIds})
-    // }, [sorting, selectionDispatch, sortingInfo, showAcceptedOnly, hideRejected, restrictToVisibleElectrodes, visibleElectrodeIds, peakElectrodeIds, curation])
+    // the following was causing a problem
+    useEffect(() => {
+        const includeUnit = (uid: number) => {
+            // if ((restrictToVisibleElectrodes) && (visibleElectrodeIds) && (peakElectrodeIds)) {
+            //     const peakElectrodeId = peakElectrodeIds[uid + '']
+            //     if (!visibleElectrodeIds.includes(peakElectrodeId)) return false
+            // }
+            if (showAcceptedOnly) {
+                return getLabelsForUnitId(uid, curation).includes('accept')
+            }
+            else if (hideRejected) {
+                return !getLabelsForUnitId(uid, curation).includes('reject')
+            }
+            else {
+                return true
+            }    
+        }
+        const visibleUnitIds = (sortingInfo?.unit_ids || []).filter(uid => includeUnit(uid))
+        selectionDispatch({type: 'SetVisibleUnitIds', visibleUnitIds})
+    }, [sortingInfo, selectionDispatch, showAcceptedOnly, hideRejected, visibleElectrodeIds, curation])
 
     return (
         <div>
             <span style={{whiteSpace: 'nowrap'}}><Checkbox checked={showAcceptedOnly} onClick={handleShowAcceptedOnly}/> Show accepted only</span>
             <span style={{whiteSpace: 'nowrap'}}><Checkbox checked={hideRejected} onClick={handleHideRejected} disabled={showAcceptedOnly}/> Hide rejected</span>
-            <span style={{whiteSpace: 'nowrap'}}><Checkbox checked={restrictToVisibleElectrodes} onClick={handleRestrictToVisibleElectrodes} disabled={!visibleElectrodeIds}/> Restrict to visible electrodes</span>
+            {/* <span style={{whiteSpace: 'nowrap'}}><Checkbox checked={restrictToVisibleElectrodes} onClick={handleRestrictToVisibleElectrodes} disabled={!visibleElectrodeIds}/> Restrict to visible electrodes</span> */}
         </div>
     )
 }
 
-// const getLabelsForUnitId = (unitId: number, curation: SortingCuration) => {
-//     const labelsByUnit = (curation || {}).labelsByUnit || {};
-//     return labelsByUnit[unitId] || []
-// }
+const getLabelsForUnitId = (unitId: number, curation: SortingCuration) => {
+    const labelsByUnit = (curation || {}).labelsByUnit || {};
+    return labelsByUnit[unitId] || []
+}
 
 export default VisibleUnitsControl
