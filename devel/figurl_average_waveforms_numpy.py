0b100f7492244590af033b51c334742633e8f6c8
Jeremy Magland <jeremy.magland@gmail.com>
2021-08-05 12:30:33 -0400

    support numpy arrays in avg-waveforms-numpy

--- devel/figurl_average_waveforms_numpy.py
+++ devel/figurl_average_waveforms_numpy.py
@@ -1,12 +1,17 @@
+import numpy as np
 from typing import Any, List
 import kachery_client as kc
+from sortingview.serialize_wrapper import _serialize
+
+def store_json(x: dict):
+    return kc.store_json(_serialize(x))
 
 def figurl_average_waveforms_numpy(*, channel: str, waveforms: List[Any], electrode_channels: List[Any]):
     """
     Generate a sortingview url that shows the average waveforms numpy page
     """
     base_url = 'http://localhost:3000'
-    object_uri = kc.store_json({
+    object_uri = store_json({
         'type': 'sortingview.average-waveforms-numpy.1',
         'data': {
             'waveforms': waveforms,
@@ -20,23 +25,23 @@ def figurl_average_waveforms_numpy(*, channel: str, waveforms: List[Any], electr
 waveforms = [
     {
         'unitId': 1,
-        'channelIds': [1, 2, 3, 4],
-        'waveform': [
+        'channelIds': np.array([1, 2, 3, 4], dtype=np.int32),
+        'waveform': np.array([
             [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1],
             [1, 2, 3, 4, 5, 6, -5, -4, -3, -2, -1],
             [-1, -2, -3, -4, -5, -6, -5, -4, -3, -2, -1],
             [-1, -2, -3, -4, -5, -6, 5, 4, 3, 2, 1]
-        ]
+        ], dtype=np.float32)
     },
     {
         'unitId': 2,
-        'channelIds': [1, 2, 3, 4],
-        'waveform': [
+        'channelIds': np.array([1, 2, 3, 4], dtype=np.int32),
+        'waveform': np.array([
             [0, 0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, 0, 0],
             [0, 0, 0, 1, 2, 3, 4, 5, 6, -5, -4, -3, -2, -1, 0, 0, 0],
             [0, 0, 0, -1, -2, -3, -4, -5, -6, -5, -4, -3, -2, -1, 0, 0, 0],
             [0, 0, 0, -1, -2, -3, -4, -5, -6, 5, 4, 3, 2, 1, 0, 0, 0]
-        ]
+        ], dtype=np.float32)
     }
 ]
 electrode_channels = [
