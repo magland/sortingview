c4c3cf5028061bd9ea4350e8335478f41bc86378
Jeff Soules <jsoules@flatironinstitute.org>
2021-08-27 15:33:40 -0400

    Further memoization in UnitsTable, TableWidget: select-all time down to ~110 ms for 260 units.

--- src/python/sortingview/gui/extensions/unitstable/Units/UnitsTable.tsx
+++ src/python/sortingview/gui/extensions/unitstable/Units/UnitsTable.tsx
@@ -2,8 +2,8 @@ import { useChannel, usePureCalculationTask } from 'kachery-react';
 import sortByPriority from 'labbox-react/extensionSystem/sortByPriority';
 import { ExternalSortingUnitMetric } from 'python/sortingview/gui/pluginInterface/Sorting';
 import { SortingComparisonUnitMetricPlugin } from 'python/sortingview/gui/pluginInterface/SortingComparisonUnitMetricPlugin';
-import React, { FunctionComponent, useCallback } from 'react';
-import { mergeGroupForUnitId, Sorting, SortingCuration, SortingSelection, SortingSelectionDispatch, SortingUnitMetricPlugin } from "../../../pluginInterface";
+import React, { FunctionComponent, useCallback, useMemo } from 'react';
+import { mergeGroupForUnitId, Sorting, SortingCuration, SortingSelectionDispatch, SortingUnitMetricPlugin } from "../../../pluginInterface";
 import '../unitstable.css';
 import TableWidget, { Column, Row } from './TableWidget';
 
@@ -12,9 +12,9 @@ interface Props {
     sortingComparisonUnitMetrics?: SortingComparisonUnitMetricPlugin[]
     units: number[]
     metrics?: {[key: string]: {data: {[key: string]: any}, error: string | null}}
-    selection: SortingSelection
+    selectedUnitIds?: number[]
     selectionDispatch: SortingSelectionDispatch
-    sorting: Sorting
+    unitMetricsUri?: string
     compareSorting?: Sorting
     curation: SortingCuration
     height?: number
@@ -22,13 +22,86 @@ interface Props {
     sortingSelector?: string
 }
 
+const unitIdStyle: React.CSSProperties = {
+    color: 'black',
+    fontWeight: 'bold',
+    cursor: 'pointer'
+}
+const labelStyle: React.CSSProperties = {
+    color: 'gray',
+    textDecoration: 'underline',
+    cursor: 'pointer'
+}
+const labelsElement = (x: any) => {
+    const y = x as string[]
+    return (
+        <span>
+            {
+                y.map(label => (
+                    <span key={label}><span style={labelStyle}>{label}</span>&nbsp;</span>
+                ))
+            }
+        </span>
+    )
+}
+
+const alphaSort = (a: any, b: any) => {
+    return (a < b) ? -1 : (a > b) ? 1 : 0
+}
+const numericSort = (a: any, b: any) => {
+    return (Number(a) - Number(b))
+}
+const formatNumberForDisplay = (x: number) => {
+    if (!x) return x
+    if (Number.isInteger(x)) return x + '' // if integer, display the whole thing
+    try {
+        return x.toPrecision(4) + '' // otherwise, give 4 significant digits
+    }
+    catch {
+        return x
+    }
+}
+const numericElement = (x: any) => (<span>{formatNumberForDisplay(x as number)}</span>)
+
+const unitIdElement = (x: any, sortingSelector: string) => {
+    const {unitId, mergeGroup} = x as {unitId: number, mergeGroup: number[] | null}
+    return (
+        <span>
+            <span key="unitId" style={unitIdStyle}>
+                {unitId + '' + sortingSelector}
+            </span>
+            {
+                ((mergeGroup) && (mergeGroup.length > 0)) && (
+                    <span key="mergeGroup">{` (${mergeGroup.map(id => (id + '' + sortingSelector)).join(", ")})`}</span>
+                )
+            }
+        </span>
+    )
+}
+
+const getLabelsForUnitId = (unitId: number, curation: SortingCuration) => {
+    const labelsByUnit = (curation || {}).labelsByUnit || {};
+    return labelsByUnit[unitId] || []
+}
+
+
 const UnitsTable: FunctionComponent<Props> = (props) => {
-    const { sorting, sortingUnitMetrics, sortingComparisonUnitMetrics, units, metrics, selection, selectionDispatch, curation, height, selectionDisabled, sortingSelector } = props
-    const selectedUnitIds = ((selection || {}).selectedUnitIds || [])
-    const sortingUnitMetricsList = sortByPriority(Object.values(sortingUnitMetrics || {})).filter(p => (!p.disabled))
-    const sortingComparisonUnitMetricsList = sortByPriority(Object.values(sortingComparisonUnitMetrics || {})).filter(p => (!p.disabled))
+    const { unitMetricsUri, sortingUnitMetrics, sortingComparisonUnitMetrics, units, metrics, selectedUnitIds, selectionDispatch, curation, height, selectionDisabled, sortingSelector } = props
+    const selectedRowIds = (selectedUnitIds || []).map(unitId => (unitId + ''))
+
+    // Prepare lists of metrics.
+    // These memoizations work b/c the parameters should be memoized in the caller.
+    const sortingUnitMetricsList = useMemo(() => {
+        return sortByPriority(Object.values(sortingUnitMetrics || {})).filter(p => (!p.disabled))
+    }, [sortingUnitMetrics])
+    const sortingComparisonUnitMetricsList = useMemo(() => {
+        return sortByPriority(Object.values(sortingComparisonUnitMetrics || {})).filter(p => (!p.disabled))
+    }, [sortingComparisonUnitMetrics])
     // If we are in a comparison between sortings, use both sortingUnitMetricList and sortingComparisonUnitMetricList. Otherwise, use only the sortingUnitMetricList 
-    const allUnitMetricsList = sortingSelector ? [...sortingComparisonUnitMetricsList, ...sortingUnitMetricsList] : sortingUnitMetricsList
+    const allUnitMetricsList = useMemo(
+        () => (sortingSelector ? [...sortingComparisonUnitMetricsList, ...sortingUnitMetricsList] : sortingUnitMetricsList),
+        [sortingSelector, sortingComparisonUnitMetricsList, sortingUnitMetricsList]
+    )
 
     const handleSelectedRowIdsChanged = useCallback((selectedRowIds: string[]) => {
         selectionDispatch({
@@ -37,153 +110,110 @@ const UnitsTable: FunctionComponent<Props> = (props) => {
         })
     }, [ selectionDispatch ])
 
-    const rows: Row[] = units.map(unitId => ({
-        rowId: unitId + '',
-        data: {}
-    }))
+    const _metrics = useMemo(() => metrics || {}, [metrics])
 
-    const numericSort = (a: any, b: any) => {
-        return (Number(a) - Number(b))
-    }
-    const formatNumberForDisplay = (x: number) => {
-        if (!x) return x
-        if (Number.isInteger(x)) return x + '' // if integer, display the whole thing
-        try {
-            return x.toPrecision(4) + '' // otherwise, give 4 significant digits
-        }
-        catch {
-            return x
-        }
-    }
-    const numericElement = (x: any) => (<span>{formatNumberForDisplay(x as number)}</span>)
-    const unitIdStyle: React.CSSProperties = {
-        color: 'black',
-        fontWeight: 'bold',
-        cursor: 'pointer'
-    }
-    const ss = sortingSelector || ''
-    const unitIdElement = (x: any) => {
-        const {unitId, mergeGroup} = x as {unitId: number, mergeGroup: number[] | null}
-        return (
-            <span>
-                <span key="unitId" style={unitIdStyle}>
-                    {unitId + '' + ss}
-                </span>
-                {
-                    ((mergeGroup) && (mergeGroup.length > 0)) && (
-                        <span key="mergeGroup">{` (${mergeGroup.map(id => (id + '' + ss)).join(", ")})`}</span>
-                    )
-                }
-            </span>
-        )
-    }
+    const _unitIdElement = useCallback((x) => unitIdElement(x, sortingSelector || ''), [sortingSelector])
 
-    const alphaSort = (a: any, b: any) => {
-        return (a < b) ? -1 : (a > b) ? 1 : 0
-    }
-    const labelStyle: React.CSSProperties = {
-        color: 'gray',
-        textDecoration: 'underline',
-        cursor: 'pointer'
-    }
-    const labelsElement = (x: any) => {
-        const y = x as string[]
-        return (
-            <span>
-                {
-                    y.map(label => (
-                        <span key={label}><span style={labelStyle}>{label}</span>&nbsp;</span>
-                    ))
-                }
-            </span>
-        )
-    }
-
-    const columns: Column[] = []
-    
-    // first column (Unit ID)
-    columns.push({
-        columnName: '_unit_id',
-        label: 'Unit ID',
-        tooltip: 'Unit ID',
-        sort: numericSort,
-        dataElement: unitIdElement
-    })
-    rows.forEach(row => {
-        const unitId = Number(row.rowId)
-        row.data['_unit_id'] = {
-            value: {unitId, mergeGroup: mergeGroupForUnitId(unitId, curation)},
-            sortValue: unitId
-        }
-    })
-
-    // second column (Labels)
-    columns.push({
-        columnName: '_labels',
-        label: 'Labels',
-        tooltip: 'Curation labels',
-        sort: alphaSort,
-        dataElement: labelsElement
-    })
-    rows.forEach(row => {
-        const unitId = Number(row.rowId)
-        const labels = getLabelsForUnitId(unitId, curation)
-        row.data['_labels'] = {
-            value: labels,
-            sortValue: labels.join(', ')
-        }
-    })
-
+    // Fetch external metrics.
     const {channelName} = useChannel()
-    const {returnValue: externalUnitMetrics} = usePureCalculationTask<ExternalSortingUnitMetric[]>(sorting.unitMetricsUri ? 'fetch_unit_metrics.1' : undefined, {unit_metrics_uri: sorting.unitMetricsUri || ''}, {channelName})
+    const {returnValue: externalUnitMetrics} = usePureCalculationTask<ExternalSortingUnitMetric[]>(unitMetricsUri ? 'fetch_unit_metrics.1' : undefined, {unit_metrics_uri: unitMetricsUri || ''}, {channelName})
 
-    ;(externalUnitMetrics || []).forEach((m: ExternalSortingUnitMetric) => {
-        const columnName = 'external-metric-' + m.name
-        columns.push({
-            columnName,
-            label: m.label,
-            tooltip: m.tooltip || '',
+    // Assemble column list
+    // -- Static columns (unit id & labels)
+    const unitColumn = useMemo(() => {
+        return ({
+            columnName: '_unit_id',
+            label: 'Unit ID',
+            tooltip: 'Unit ID',
             sort: numericSort,
-            dataElement: numericElement
+            dataElement: _unitIdElement
         })
-        rows.forEach(row => {
-            const unitId = Number(row.rowId)
-            const v = m.data[unitId + '']
-            row.data[columnName] = {
-                value: v !== undefined ? v : NaN,
-                sortValue: v !== undefined ? v : NaN
-            }
-        })
-    })
+    }, [_unitIdElement])
 
-    ;(allUnitMetricsList).forEach((m: SortingUnitMetricPlugin | SortingComparisonUnitMetricPlugin) => {
-        const columnName = 'plugin-metric-' + m.name
-        const metric = (metrics || {})[m.name] || null
-        const metricData = metric ? metric.data : null
-        columns.push({
-            columnName,
-            label: m.columnLabel,
-            tooltip: m.tooltip || '',
-            sort: numericSort,
-            dataElement: m.component,
-            calculating: (metric && (!metricData))
+    const labelColumn = useMemo(() => {
+        return ({
+            columnName: '_labels',
+            label: 'Labels',
+            tooltip: 'Curation labels',
+            sort: alphaSort,
+            dataElement: labelsElement
         })
-        
-        rows.forEach(row => {
-            const unitId = Number(row.rowId)
-            const record = metricData ? (
-                (unitId + '' in metricData) ? metricData[unitId + ''] : undefined
-            ) : undefined
-            const v = (record !== undefined) ? m.getValue(record) : undefined
-            row.data[columnName] = {
-                value: record,
-                sortValue: v !== undefined ? v : (m.isNumeric ? NaN : '')
-            }
-        })
-    })
+    }, [])
 
-    const selectedRowIds = selectedUnitIds.map(unitId => (unitId + ''))
-   
+    const columns: Column[] = useMemo(() => {
+        const externalMetricColumns = (externalUnitMetrics || []).map(
+            (m) => ({
+                columnName: 'external-metric-' + m.name,
+                label: m.label,
+                tooltip: m.tooltip || '',
+                sort: numericSort,
+                dataElement: numericElement
+            })
+        )
+        const internalMetricColumns = (allUnitMetricsList || []).map(
+            (m) => {
+                const metric = _metrics[m.name] || null
+                return ({
+                    columnName: 'plugin-metric-' + m.name,
+                    label: m.columnLabel,
+                    tooltip: m.tooltip || '',
+                    sort: numericSort,
+                    dataElement: m.component,
+                    calculating: (metric && !metric?.data)
+            })}
+        )
+
+        return [unitColumn, labelColumn, ...externalMetricColumns, ...internalMetricColumns]
+    }, [unitColumn, labelColumn, externalUnitMetrics, allUnitMetricsList, _metrics])
+
+    // Given known columns, assemble rows.
+
+    const rows: Row[] = useMemo(() => {
+        return units.map((unitId) => {
+            const unitIdData = {
+                value: {unitId, mergeGroup: mergeGroupForUnitId(unitId, curation)},
+                sortValue: unitId
+            }
+            const labels = getLabelsForUnitId(unitId, curation)
+            const labelData = {
+                value: labels,
+                sortValue: labels.join(', ')
+            }
+            const unitIdStr = unitId + ''
+            const externalMetricsData = (externalUnitMetrics || []).reduce((data, m) => {
+                const value = m.data[unitIdStr]
+                return {
+                    ...data,
+                    ['external-metric-' + m.name]: {
+                        value: value ?? '', // Or 'NaN' to display 'NaN' text
+                        sortValue: value ?? NaN
+                    }
+                }
+            }, {})
+            const internalMetricsData = (allUnitMetricsList || []).reduce((data, m) => {
+                    const unitRecord = _metrics[m.name]?.data?.[unitIdStr]
+                    const value = unitRecord && m.getValue(unitRecord)
+                    return {
+                        ...data,
+                        ['plugin-metric-' + m.name]: {
+                            value: unitRecord,
+                            sortValue: value ?? (m.isNumeric ? NaN : '')
+                        }
+                    }
+                }, {})
+
+            return ({
+                rowId: unitId + '',
+                data: {
+                    '_unit_id': unitIdData,
+                    '_labels': labelData,
+                    ...externalMetricsData,
+                    ...internalMetricsData
+                }
+            })
+        })
+    }, [units, curation, externalUnitMetrics, _metrics, allUnitMetricsList])
+
     return (
         <TableWidget
             rows={rows}
@@ -196,37 +226,5 @@ const UnitsTable: FunctionComponent<Props> = (props) => {
         />
     )
 }
-
-const getLabelsForUnitId = (unitId: number, curation: SortingCuration) => {
-    const labelsByUnit = (curation || {}).labelsByUnit || {};
-    return labelsByUnit[unitId] || []
-}
-
-// const UnitIdCell = React.memo((props: {id: number, mergeGroup: number[] | null, sortingId: string}) => {
-//     const g = props.mergeGroup
-//     return <TableCell><span>{props.id + ''}{g && ' (' + [...g].sort().join(', ') + ')'}</span></TableCell>
-// })
-
-// const UnitLabelCell = React.memo((props: {labels: string}) => (
-//     <TableCell><span>{props.labels}</span></TableCell>
-// ));
-
-// const MetricCell = React.memo((a: {title?: string, error: string, data: any, PayloadComponent: React.ComponentType<{record: any}>}) => {
-//     const { error, data, PayloadComponent } = a
-//     if (error !== '') {
-//         return (<TableCell><span>{`Error: ${error}`}</span></TableCell>);
-//     }
-//     if (data === null || data === '') { // 0 is a valid value!!
-//         return (<TableCell><LinearProgress style={{'width': '60%'}}/></TableCell>);
-//     } else {
-//         return (
-//             <TableCell>
-//                 <span title={a.title}>
-//                     <PayloadComponent record = {data} />
-//                 </span>
-//             </TableCell>
-//         );
-//     }
-// });
 
 export default UnitsTable
