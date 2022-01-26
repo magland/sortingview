d853fbf8af8714e5c32da65538e96a78e7394599
Jeremy Magland <jeremy.magland@gmail.com>
2022-01-26 11:19:03 -0500

    remove refs to workspace_list

--- sortingview/tasks/__init__.py
+++ sortingview/tasks/__init__.py
@@ -3,12 +3,10 @@ from .example_recording_sorting import *
 from .recording_info import *
 from .sorting_info import *
 from .preload_extract_snippets import *
-from .workspace_list_subfeed import *
 from .workspace_action import *
 from .workspace_sorting_curation_action import *
 from .latency_test import *
 from .python_package_version import *
-from .get_workspace_list import *
 from .get_experitime_timeseries_samples import *
 from .get_snippets_for_uri import *
 from .ssv_sorting_curation_action import *
