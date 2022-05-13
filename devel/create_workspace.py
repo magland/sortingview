1c59f77bbf0f56738371e7382997707da42c9376
Jeremy Magland <jmagland@flatironinstitute.org>
2022-05-13 08:45:52 -0400

    print workspace uri

--- devel/create_workspace.py
+++ devel/create_workspace.py
@@ -9,6 +9,7 @@ S = sv.LabboxEphysSortingExtractor.from_memory(sorting, serialize=True)
 W = sv.create_workspace()
 recording_id = W.add_recording(label='recording1', recording=R)
 sorting_id = W.add_sorting(recording_id=recording_id, label='true', sorting=S)
+print(W.uri)
 
 W.set_sorting_curation_authorized_users(sorting_id=sorting_id, user_ids=['jmagland@flatironinstitute.org'])
 url2 = W.spikesortingview(recording_id=recording_id, sorting_id=sorting_id, label='Test workspace', include_curation=True)
