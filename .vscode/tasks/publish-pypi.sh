29892df675850ffd0fac2727cc0b60b7f96bc5c4
Jeremy Magland <jmagland@flatironinstitute.org>
2022-09-26 09:11:10 -0400

    use mainlayout, but remove curation

--- .vscode/tasks/publish-pypi.sh
+++ .vscode/tasks/publish-pypi.sh
@@ -44,6 +44,6 @@ done
 twine upload ./dist/*
 
 # Tag this commit
-git tag v0.10.8
+git tag v0.10.9
 
-echo "Tagged as v0.10.8"
+echo "Tagged as v0.10.9"
--- sortingview/views/View.py
+++ sortingview/views/View.py
@@ -73,7 +73,7 @@ class View:
         if self.is_layout:
             all_views = self.get_descendant_views_including_self()
             data = {
-                'type': 'SortingLayout',
+                'type': 'MainLayout',
                 'layout': self.to_dict(),
                 'views': [
                     {
@@ -84,10 +84,10 @@ class View:
                     for view in all_views if not view.is_layout
                 ]
             }
-            if sorting_curation_uri is not None:
-                data['sortingCurationUri'] = sorting_curation_uri
-                if project_id is None:
-                    project_id = kcl.get_project_id()
+            # if sorting_curation_uri is not None:
+            #     data['sortingCurationUri'] = sorting_curation_uri
+            #     if project_id is None:
+            #         project_id = kcl.get_project_id()
             view_url = os.getenv('SORTINGVIEW_VIEW_URL', 'gs://figurl/spikesortingview-9')
             F = fig.Figure(view_url=view_url, data=data)
             url = F.url(label=label, project_id=project_id, local=local)
