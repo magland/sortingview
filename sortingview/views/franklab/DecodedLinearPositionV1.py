373cce4cccdd5f1de8f0bf219d33b8d36f17538d
Jeff Soules <jsoules@flatironinstitute.org>
2022-11-02 14:19:17 -0400

    Include support for linear 1d decode data type.

--- sortingview/views/franklab/DecodedLinearPositionV1.py
+++ sortingview/views/franklab/DecodedLinearPositionV1.py
@@ -4,14 +4,23 @@ from ..View import View
 
 class DecodedLinearPositionData(View):
     def __init__(self, *,
-        values: np.array,
-        positions: np.array,
-        frame_bounds: np.array,
-        sampling_frequency: Optional[float]
+        values: np.ndarray,
+        positions: np.ndarray,
+        frame_bounds: np.ndarray,
+        positions_key: np.ndarray,
+        observed_positions: Optional[np.ndarray],
+        start_time_sec: Optional[float],
+        sampling_frequency: Optional[float],
+        **kwargs
     ) -> None:
+        super().__init__('DecodedLinearPositionData', **kwargs)
         self.values = values
         self.positions = positions
         self.frame_bounds = frame_bounds
+        self.positions_key = positions_key
+        if (observed_positions is not None):
+            self.observed_positions = observed_positions.astype('float32')
+        self.start_time_sec = 0 if start_time_sec == None else start_time_sec
         self.sampling_frequency = sampling_frequency
 
     def to_dict(self) -> dict:
@@ -20,8 +29,12 @@ class DecodedLinearPositionData(View):
             'values': self.values,
             'positions': self.positions,
             'frameBounds': self.frame_bounds,
+            'positionsKey': self.positions_key,
+            'startTimeSec': self.start_time_sec,
             'samplingFrequencyHz': self.sampling_frequency
         }
+        if self.observed_positions is not None:
+            ret['observedPositions'] = self.observed_positions
         return ret
 
     def child_views(self) -> List[View]:
--- sortingview/views/franklab/__init__.py
+++ sortingview/views/franklab/__init__.py
@@ -1,1 +1,2 @@
 from .TrackPositionAnimationV1 import TrackPositionAnimationV1, DecodedPositionData
+from .DecodedLinearPositionV1 import DecodedLinearPositionData
