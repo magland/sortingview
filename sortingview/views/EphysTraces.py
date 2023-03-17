0ffb2ca22089d5bd412ec229dc92f2ed23cf99e4
Jeremy Magland <jeremy.magland@gmail.com>
2023-03-17 09:09:27 -0400

    sorting_uri for ephys traces

--- sortingview/views/EphysTraces.py
+++ sortingview/views/EphysTraces.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from .View import View
 
 
@@ -9,17 +9,21 @@ class EphysTraces(View):
     def __init__(self, *,
         format: str, # spikeinterface.binary
         uri: str,
+        sorting_uri: Union[str, None]=None,
         **kwargs
     ) -> None:
         super().__init__('EphysTraces', **kwargs)
         self._format = format
         self._uri = uri
+        self._sorting_uri = sorting_uri
     def to_dict(self) -> dict:
         ret = {
             'type': self.type,
             'format': self._format,
             'uri': self._uri
         }
+        if self._sorting_uri is not None:
+            ret['sortingUri'] = self._sorting_uri
         return ret
     def child_views(self) -> List[View]:
         return []
