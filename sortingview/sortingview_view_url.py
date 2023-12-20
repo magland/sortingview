bd2d7e3ffcf75dc827f9bb0ddbb422b6a1931ee4
Jeremy Magland <jeremy.magland@gmail.com>
2023-12-20 17:49:06 -0500

    use npm package

--- sortingview/sortingview_view_url.py
+++ sortingview/sortingview_view_url.py
@@ -1,4 +1,4 @@
 import os
 
 
-sortingview_view_url = os.getenv("SORTINGVIEW_VIEW_URL", "gs://figurl/sortingview-11")
+sortingview_view_url = os.getenv("SORTINGVIEW_VIEW_URL", "npm://@fi-sci/figurl-sortingview@12/dist")
